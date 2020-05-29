a2f540437e1787fa24f8febae1dfbcfd799a3cec
soyalatte <soyalatte@neowiz.com>
2020-05-29 13:37:38 +0900

    Updates

---
+++ precache-manifest.fa1ce15b682f671227181428dd3ed5d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b303bfa8f64bbd9290fc2ab994a949",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "321409048070df9fc220",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8f6d1c60b14cce7671f",
+    "url": "/deploy-test/static/js/2.6a6920ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-test/static/js/2.6a6920ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321409048070df9fc220",
+    "url": "/deploy-test/static/js/main.42ba89b9.chunk.js"
+  },
+  {
+    "revision": "18d941b67f37aa2a2e12",
+    "url": "/deploy-test/static/js/runtime-main.f4873b44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
